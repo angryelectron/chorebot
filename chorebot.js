02278927e2d3740d1988c30213f2d741c4fb85b9
Andrew Bythell <abythell@ziptrek.com>
2017-07-07 16:04:17 -0700

    first working version

---
+++ chorebot.js
@@ -0,0 +1,26 @@
+const Twit = require('twit')
+const Chores = require('./lib/chores.js')
+const CronJob = require('cron').CronJob
+const winston = require('winston')
+const chores = new Chores()
+const credentials = require('./credentials.json')
+const twitter = new Twit(credentials)
+
+winston.add(winston.transports.File, {
+  filename: 'chorebot.log',
+  level: 'info',
+  timestamp: true
+})
+winston.info('Starting Chorebot.')
+let job = new CronJob('0 0 16 * * *', postAChore)
+job.start()
+
+function postAChore () {
+  return twitter.post('statuses/update', {status: chores.get()}, (err, data) => {
+    if (err) {
+      winston.error(err)
+    } else {
+      winston.info(data.text)
+    }
+  })
+}
